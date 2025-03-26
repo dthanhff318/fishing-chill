fbd4ec4e7e7ab634efbefd1a8204819b5596e1d8
dthanhff <dthanhff318@gmail.com>
2025-03-26 22:00:48 +0700

    fist-commit

---
+++ src/components/playerHud/playerHud.tsx
@@ -0,0 +1,11 @@
+import PlayerHUDStyle from "./playerHud.style";
+
+const PlayerHUD = () => {
+  return (
+    <PlayerHUDStyle>
+      <img className="hud-background" src="/player-banner.png" alt="" />
+    </PlayerHUDStyle>
+  );
+};
+
+export default PlayerHUD;
---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,11 @@
+import PlayerHUD from "../../components/playerHud/playerHud";
+
+const Home = () => {
+  return (
+    <div>
+      <PlayerHUD />
+    </div>
+  );
+};
+
+export default Home;
